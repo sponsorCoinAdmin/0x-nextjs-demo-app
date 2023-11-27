e48b06dfeb75eedb33560d1c7ddd3fad0e80fb8a
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-11-27 01:16:15 -0500

    WIP

--- app/0X/page.tsx
+++ app/0X/page.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import Swap from './Swap'
+'use client'
+import { useState } from "react";
+import PriceView from "./Price";
+import QuoteView from "./Quote";
+import type { PriceResponse } from "./api/types";
+import { useAccount } from "wagmi";
 
-function swap() {
+export default function Home() {
+  const [tradeDirection, setTradeDirection] = useState("sell");
+  const [finalize, setFinalize] = useState(false);
+  const [price, setPrice] = useState<PriceResponse | undefined>();
+  const [quote, setQuote] = useState();
+  const { address } = useAccount();
+
   return (
-    <div className="mainApp"><h1>0X Page</h1></div>
-  )
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
+    >
+      {finalize && price ? (
+        <QuoteView
+          takerAddress={address}
+          price={price}
+          quote={quote}
+          setQuote={setQuote}
+        />
+      ) : (
+        <PriceView
+          takerAddress={address}
+          price={price}
+          setPrice={setPrice}
+          setFinalize={setFinalize}
+        />
+      )}
+    </main>
+  );
 }
-
-export default swap
